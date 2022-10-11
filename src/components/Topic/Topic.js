9085ef3c9fb53867109772bb791141e44ff644d4
Rakib86547 <contact.antor.bd@gmail.com>
2022-10-11 10:48:44 +0600

    hero section adde

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Topic = () => {
     return (
-        <div>
-            <h1>topic</h1>
+        <div className='mt-8'>
+            <h1 className='text-3xl font-semibold'>Welcome to Quiz Maker Session</h1>
+            <p className='mt-2'>
+                You have four Options for quiz. You can Chose any quiz to <br /> the below quiz option. if your quiz answer is right you got prise. 
+            </p>
         </div>
     );
 };

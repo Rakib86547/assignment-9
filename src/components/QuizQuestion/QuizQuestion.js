ee2a83234964c3e69373495cb92ec07e16240812
Rakib86547 <contact.antor.bd@gmail.com>
2022-10-11 13:28:23 +0600

    icon added

--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -1,15 +1,37 @@
 import React from 'react';
-
-const QuizQuestion = ({qus}) => {
+import { EyeIcon } from '@heroicons/react/24/solid'
+const QuizQuestion = ({ qus }) => {
     console.log(qus);
-    const {question, options} = qus;
+    const { question, options } = qus;
     return (
-        <div>
-           <h1>quiz: {question}</h1>
-           <h4>{options[0]}</h4>
-           <h4>{options[1]}</h4>
-           <h4>{options[2]}</h4>
-           <h4>{options[3]}</h4>
+        <div className='border rounded-md border-orange-600 my-5'>
+            <div className='flex w-[100%] justify-between px-4 items-center'>
+                <h1 className='text-3xl font-semibold text-rose-500'>quiz: {question}</h1>
+                <div>
+                    <EyeIcon className='w-6 h-6 cursor-pointer'></EyeIcon>
+                </div>
+            </div>
+            <div className='w-[50%] my-4 m-auto text-1xl'>
+                <div className='flex'>
+                    <input type="checkbox" />
+                    <h2 className='ml-3'>{options[0]}</h2>
+                </div>
+
+                <div className='flex '>
+                    <input type="checkbox" />
+                    <h2 className='ml-3 tex-2xl'>{options[1]}</h2>
+                </div>
+
+                <div className='flex'>
+                    <input type="checkbox" />
+                    <h2 className='ml-3 tex-2xl'>{options[2]}</h2>
+                </div>
+
+                <div className='flex'>
+                    <input type="checkbox" />
+                    <h2 className='ml-3 tex-2xl'>{options[3]}</h2>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Quizs/Quizs.js
+++ src/components/Quizs/Quizs.js
@@ -7,13 +7,15 @@ const Quizs = () => {
     const {name, questions} = quizData;
     return (
         <div>
-            <h1>Quiz of {name}</h1>
-            {
+            <h1 className='text-3xl font-bold py-5 text-rose-600'>Quiz of {name}</h1>
+           <div className='w-[90%] m-auto'>
+           {
                 questions.map(qus => <QuizQuestion
                 key={qus.id}
                 qus={qus}
                 ></QuizQuestion>)
             }
+           </div>
         </div>
         
     );

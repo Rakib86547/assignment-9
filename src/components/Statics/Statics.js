4bc9bf328ec0179573ee1c2a6776587d0f923011
Rakib86547 <contact.antor.bd@gmail.com>
2022-10-11 19:45:15 +0600

    chart added

--- src/components/Statics/Statics.js
+++ src/components/Statics/Statics.js
@@ -32,17 +32,15 @@ const Statics = () => {
    
 
     return (
-        <div>
+        <div className='flex justify-center items-center md:w-[100%] mt-8'>
            
-            <LineChart width={730} height={250} data={data}
-                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <LineChart width={300} height={350} data={data}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
                 <Legend />
                 <Line type="monotone" dataKey="total" stroke="#8884d8" />
-                {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
             </LineChart>
         </div>
 
